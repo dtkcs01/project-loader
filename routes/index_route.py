2a4da96510b1d2c403e32e22cd409034bb48146a
dtkcs01 <dtkcs01@gmail.com>
2020-04-24 20:19:17 +0530

    fix

--- routes/index_route.py
+++ routes/index_route.py
@@ -10,32 +10,35 @@ from flask import render_template
 index = Blueprint('index', __name__)
 
 def generate_tragets(base, dir_contents):
-    targets = [
-        [content , ('_blank' if os.path.isfile(os.path.join(base, content)) else '_self')]
-        for content in dir_contents
-    ]
+    targets = []
+    for content in dir_contents:
+        row = []
+        isfile = os.path.isfile(os.path.join(base, content))
+        row.append(content)
+        row.append('_blank' if isfile else '_self')
+        row.append('file' if isfile else 'folder')
+        targets.append(row)
     return targets
 
 @index.route('/', methods=['GET'])
 def home():
-    if('DIR' not in session):
-        session['DIR'] = pathlib.Path.home().__str__()
     if(request.method == 'GET'):
-        dir_contents = os.listdir(session['DIR'])
+        if('location' not in request.args):
+            return redirect(url_for('index.home', location = pathlib.Path.home().__str__()))
+        dir_contents = os.listdir(request.args.get('location'))
         dir_contents.insert(0, '..')
         return render_template(
             'index.html',
-            location = session['DIR'],
-            dir_contents = generate_tragets(session['DIR'], dir_contents)
+            location = request.args.get('location'),
+            dir_contents = generate_tragets(request.args.get('location'), dir_contents)
         )
 
 @index.route('/handle/<name>', methods = ['GET'])
 def add(name):
     name = name[5:]
     if(request.method == 'GET'):
-        path = os.path.join(session['DIR'], name)
+        path = os.path.abspath(os.path.join(request.args.get('location'), name))
         if(os.path.isfile(path)):
             return 'FILE'
         else:
-            session['DIR'] = path
-            return redirect(url_for('index.home'))
+            return redirect(url_for('index.home', location = path))
