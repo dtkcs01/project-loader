bae37dbc94ec1ed111d1fca26cdd936ca3c92e48
dtkcs01 <dtkcs01@gmail.com>
2020-04-25 19:53:11 +0530

    design-1

--- routes/index_route.py
+++ routes/index_route.py
@@ -9,14 +9,34 @@ from flask import render_template
 
 index = Blueprint('index', __name__)
 
+def compute_navigation(path):
+    navigation = []
+    while(len(os.path.basename(path)) > 0):
+        navigation.insert(0, (os.path.basename(path), path))
+        path = os.path.abspath(os.path.join(path, '..'))
+    navigation.insert(0, ('', path))
+    return navigation
+
+def compute_size(s):
+    units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB']
+    i = 0
+    while(s//1024 > 0):
+        s = s//1024
+        i += 1
+    return '{} {}'.format(s, units[i])
+
 def generate_tragets(base, dir_contents):
     targets = []
     for content in dir_contents:
-        row = []
-        isfile = os.path.isfile(os.path.join(base, content))
-        row.append(content)
-        row.append('_blank' if isfile else '_self')
-        row.append('file' if isfile else 'folder')
+        path = os.path.join(base, content)
+        isfile = os.path.isfile(path)
+        row = {
+            'name' : content,
+            'is_file' : isfile,
+            'icon' : 'file' if isfile else 'folder',
+            'target' : '_blank' if isfile else '_self',
+            'size' : compute_size(os.path.getsize(path)) if isfile else '-'
+        }
         targets.append(row)
     return targets
 
@@ -30,7 +50,8 @@ def home():
         return render_template(
             'index.html',
             location = request.args.get('location'),
-            dir_contents = generate_tragets(request.args.get('location'), dir_contents)
+            dir_contents = generate_tragets(request.args.get('location'), dir_contents),
+            navigation = compute_navigation(request.args.get('location'))
         )
 
 @index.route('/handle/<name>', methods = ['GET'])
